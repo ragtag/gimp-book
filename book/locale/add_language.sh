e7cdc1c9e55bfd1d5df5cbd25a451fbd9d41bf56
ragtag <ragtag@e8c3caca-4cbd-4ef4-982e-115f28c8412e>
2013-06-02 14:16:46 +0000

    Adding helpers for translators

--- book/locale/add_language.sh
+++ book/locale/add_language.sh
@@ -4,5 +4,5 @@ echo "Pleasa enter a two letter language code you wish to create a translation f
 read translation_language
 msginit --input=book.pot --locale=$translation_language
 sed -i 's/ASCII/UTF-8/g' '$translation_language.po'
-echo "Thank you for maing an effort to translate GIMP Book."
+echo "Thank you for helping with translating GIMP Book."
 echo "You can now add your translations in the msgstr entries in the $translation_language.po file"
